65fbb5f3ad8e6282312841b1c7b88519e076b80a
busrasimsek <42219062+busrasimsek@users.noreply.github.com>
2023-07-31 14:49:24 +0300

    Create layers

---
+++ SiteManagement.Core/Controller/BaseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SiteManagement.Core.Controller
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BaseController : ControllerBase
+    {
+    }
+}
